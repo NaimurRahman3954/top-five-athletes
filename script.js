8421a478b8856cae3a1357dbb6278344c4923059
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-08-23 17:17:16 +0600

    list created with JS

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const buttons = document.querySelectorAll(".button");
 var node = document.createElement('li');
 const players = [];
 
+function display(players){
+   const listItems = document.getElementById('list');
+   listItems.innerHTML = '';
+   for (let i = 0; i < players.length; i++) {
+      const element = players[i];
+      console.log(element);
+
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+      ${element}
+      `;
+      listItems.appendChild(listItem);
+   }
+}
+
 function addToList(element){
    const playerName = element.parentNode.children[1].innerText;
    // console.log(playerName);
@@ -12,6 +27,8 @@ function addToList(element){
    // console.log(numberOfPlayers);
    // document.getElementById('no.-of-players').innerText = numberOfPlayers;
    document.getElementById('no.-of-players').innerText = players.length;
+
+   display(players);
 }
 
 buttons.forEach((item) => {
@@ -21,7 +38,7 @@ buttons.forEach((item) => {
       item.disabled = true;
 
       // add to list
-      node.appendChild(document.createTextNode('Scooter'));
-      document.querySelector('ol').appendChild(node);
+      // node.appendChild(document.createTextNode('Scooter'));
+      // document.querySelector('ol').appendChild(node);
    })            
 })
