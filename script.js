d7ba52b95e845f048372668ee914d2f83facd772
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-08-23 16:30:30 +0600

    players' names updated

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const buttons = document.querySelectorAll(".button");
 var node = document.createElement('li');
+const players = [];
+
+function addToList(element){
+   const playerName = element.parentNode.children[1].innerText;
+   // console.log(playerName);
+
+   players.push(playerName);
+   // let numberOfPlayers = document.querySelectorAll(".list li").length;
+   console.log(players);
+   // console.log(numberOfPlayers);
+   // document.getElementById('no.-of-players').innerText = numberOfPlayers;
+   document.getElementById('no.-of-players').innerText = players.length;
+}
 
 buttons.forEach((item) => {
    item.addEventListener("click", ()=>{
