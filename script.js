30f253793716f7d0e45f33b682b5c1c57c302750
NaimurRahman3954 <mnrahaman3954@gmail.com>
2022-08-23 23:07:31 +0600

    Nan handled

--- script.js
+++ script.js
@@ -68,3 +68,23 @@ managerialAmountBtn.addEventListener("click", function(){
    document.getElementById("managerPaidAmount").value = "";
    document.getElementById("coachPaidAmount").value = "";
 })
+
+
+// NaN handling
+function validate(evt) {
+   var theEvent = evt || window.event;
+ 
+   // Handle paste
+   if (theEvent.type === 'paste') {
+       key = event.clipboardData.getData('text/plain');
+   } else {
+   // Handle key press
+       var key = theEvent.keyCode || theEvent.which;
+       key = String.fromCharCode(key);
+   }
+   var regex = /[0-9]|\./;
+   if( !regex.test(key) ) {
+     theEvent.returnValue = false;
+     if(theEvent.preventDefault) theEvent.preventDefault();
+   }
+ }
